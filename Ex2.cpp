e056e7678e2d45b4fad6d8161a2c4824e698b717
FCAraujo10 <cardoso.araujo@aluno.ifsp.edu.br>
2025-04-30 19:41:22 -0300

    Add files via upload

---
+++ Ex2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int base;
+	int area;
+	
+	cout << "Informe a base do quadrado: ";
+	cin >> base;
+	
+	area = base * base;
+	
+	cout << " a area do quadrado eh: " << area << endl;
+	
+	return 0;
+	
+}
