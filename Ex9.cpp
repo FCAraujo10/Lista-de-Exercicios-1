a9e0ee2a7a7f95e8f5cbaa7b27ae1a68ef29e511
FCAraujo10 <cardoso.araujo@aluno.ifsp.edu.br>
2025-06-11 20:02:09 -0300

    Add files via upload

---
+++ Ex9.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    float d;
+    cout << "Digite o diametro do circulo: ";
+    cin >> d;
+    float r = d / 2;
+    cout << "Area: " << M_PI * r * r << endl;
+    return 0;
+}
