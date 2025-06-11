ffff1022389956c13a8036c404fae6fd7b555877
FCAraujo10 <cardoso.araujo@aluno.ifsp.edu.br>
2025-06-11 19:58:26 -0300

    Add files via upload

---
+++ Ex6.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    float a, b;
+    cout << "Digite dois valores: ";
+    cin >> a >> b;
+    cout << "Media geometrica: " << sqrt(a * b) << endl;
+    return 0;
+}
